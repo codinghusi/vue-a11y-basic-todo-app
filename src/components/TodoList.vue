0b0942ce2be920fddba58ec7328b45f5c9aab9c5
Gerrit Weiermann <gerrit.weiermann@denkwerk.com>
2024-10-09 11:07:56 +0200

    add functionality to edit button

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,32 @@
 <template>
     <ul class="w-full flex flex-col gap-2">
-        <TodoRow v-for="todo in todos" :todo="todo" :key="todo.id" @change="handleUpdate" />
+        <li class="row-container" :class="{done: todo.done}" v-for="todo in todos" :todo="todo" :key="todo.id">
+            <input type="checkbox" v-model="todo.done"/>
+
+            <span class="description" v-if="!isRowInEditMode(todo)">
+                {{ todo.description }}
+            </span>
+
+            <input
+                type="text"
+                class="border rounded px-2 description-editable"
+                v-model="editinigDescription" 
+                ref="editableDescription"
+                @keydown.enter="doneEditMode()"
+                v-else />
+            
+            <button class="pointer" v-if="!isRowInEditMode(todo)" @click="putRowInEditMode(todo)"> edit </button>
+            <template v-else>
+                <button class="pointer" @click="doneEditMode()"> done </button>
+                <button class="pointer" @click="cancelEditMode()"> cancel </button>
+            </template>
+            <button class="pointer rotate-90"> {{ '<' }} </button>
+            <button class="pointer rotate-90"> {{ '>' }} </button>
+        </li>
     </ul>
 </template>
 
 <script>
-    import TodoRow from './TodoRow.vue'
     export default {
         props: {
             todos: {
@@ -16,10 +37,47 @@
         methods: {
             handleUpdate() {
                 console.log('todo')
+            },
+            isRowInEditMode(todo) {
+                return this.rowInEditMode?.id === todo.id
+            },
+            putRowInEditMode(todo) {
+                this.rowInEditMode = todo
+                this.editinigDescription = todo.description
+                Promise.resolve().then(() => this.$refs.editableDescription[0].focus())
+            },
+            doneEditMode() {
+                this.rowInEditMode.description = this.editinigDescription
+                this.rowInEditMode = null
+            },
+            cancelEditMode() {
+                this.rowInEditMode = null
             }
         },
-        components: {
-            TodoRow
+        data() {
+            return {
+                rowInEditMode: null,
+                editinigDescription: ''
+            }
         }
     }
 </script>
+
+<style scoped>
+.row-container {
+    @apply flex flex-row gap-3;
+    @apply p-4 border rounded
+}
+
+.description {
+    @apply flex-1
+}
+
+.done > .description {
+    @apply line-through
+}
+
+.description-editable {
+    @apply flex-1
+}
+</style>
