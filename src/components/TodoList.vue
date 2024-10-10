5b41ff9085c0585831c1f9d5677ad565ccf3e8e6
Gerrit Weiermann <gerrit.weiermann@denkwerk.com>
2024-10-10 17:03:18 +0200

    add icons

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,333 +1,286 @@
 <template>
-  <div class="flex flex-col gap-3">
-    <span aria-live="polite" class="sr-only">{{ announcementMessage }}</span>
+    <div class="flex flex-col gap-3">
+        <span aria-live="polite" class="sr-only">{{ announcementMessage }}</span>
 
-    <!-- Filters -->
-    
-    <div v-if="filters" class="flex flex-row justify-end gap-4 justify-items-center content-center">
-        <label for="filters" class="py-2">Filters:</label>
-        <select v-model="setFilter" class="border rounded p-2" id="filters">
-            <option value="none">No filter</option>
-            <option value="finished" aria-label="show only finished todos">Finished</option>
-            <option value="unfinished" aria-label="show only unfinished todos">Unfinished</option>
-        </select>
+        <!-- Filters -->
+
+        <div v-if="filters" class="flex flex-row justify-end gap-4 justify-items-center content-center">
+            <label for="filters" class="py-2 cursor-pointer">Filters:</label>
+            <select v-model="setFilter" class="border rounded p-2 cursor-pointer" id="filters">
+                <option value="none">No filter</option>
+                <option value="finished" aria-label="show only finished todos">Finished</option>
+                <option value="unfinished" aria-label="show only unfinished todos">Unfinished</option>
+            </select>
+        </div>
+
+        <!-- Todo List -->
+        <ul class="w-full flex flex-col gap-2" :aria-label="`${setFilter === 'none'
+                ? 'all'
+                : setFilter === 'finished'
+                    ? 'finished'
+                    : 'unfinished'
+            } todos`">
+            <li class="row-container" :class="{ done: todo.done }" v-for="(todo, index) in filteredTodos" :todo="todo"
+                :key="todo.id">
+                <span class="sr-only">
+                    ({{ todo.done ? "finished" : "unfinished" }}) todo
+                </span>
+
+                <input type="checkbox" v-model="todo.done" class="cursor-pointer"
+                    @change="() => { emitUpdate(); announce(`marked todo as ${todo.done ? 'finished' : 'unfinished'}`) }"
+                    ref="row-start" :aria-label="todo.done ? 'mark todo as unfinished' : 'mark todo as finished'
+                        " />
+
+                <span class="description" v-if="!isRowInEditMode(todo)">
+                    {{ todo.description }}
+                </span>
+
+                <input type="text" class="border rounded px-2 description-editable" v-model="editinigDescription"
+                    ref="editableDescription" @keypress.enter="doneEditMode()" @keydown.esc="cancelEditMode()" v-else
+                    :aria-label="'description of todo ' +
+                        index +
+                        ' press enter to confirm, escape to cancel'
+                        " />
+
+                <button class="cursor-pointer" v-if="!isRowInEditMode(todo)" @click="putRowInEditMode(todo)"
+                    aria-label="edit the description of todo">
+                    <v-icon icon="edit" />
+                </button>
+                <template v-else>
+                    <button class="cursor-pointer" @click="cancelEditMode()"
+                        aria-label="cancel and restore previous description">
+                        <v-icon icon="undo" />
+                    </button>
+                    <button class="cursor-pointer" @click="doneEditMode()" aria-label="confirm your input">
+                        <v-icon icon="save" />
+                    </button>
+                </template>
+
+                <button class="cursor-pointer" :class="{ invisible: index === 0 }" @click="moveUp(index, todo)"
+                    :ref="'button-up-' + todo.id" aria-label="move entry one up">
+                    <v-icon icon="angle-up" />
+                </button>
+
+                <button class="cursor-pointer" :class="{ invisible: index === todos.length - 1 }"
+                    aria-label="move entry one down" @click="moveDown(index, todo)" :ref="'button-down-' + todo.id">
+                    <v-icon icon="angle-down" />
+                </button>
+
+                <button class="cursor-pointer" @click="deleteTodo(index, todo)" aria-label="remove entry">
+                    <v-icon icon="trash-alt" />
+                </button>
+            </li>
+        </ul>
+
+        <!-- Add Button -->
+        <div class="flex justify-end" v-if="controls">
+            <button class="border rounded p-2 cursor-pointer" @click="addRow" aria-label="add a new todo">
+                Add todo entry <v-icon icon="plus" />
+            </button>
+        </div>
     </div>
-
-    <!-- Todo List -->
-    <ul
-      class="w-full flex flex-col gap-2"
-      :aria-label="`${
-        setFilter === 'none'
-          ? 'all'
-          : setFilter === 'finished'
-          ? 'finished'
-          : 'unfinished'
-      } todos`"
-    >
-      <li
-        class="row-container"
-        :class="{ done: todo.done }"
-        v-for="(todo, index) in filteredTodos"
-        :todo="todo"
-        :key="todo.id"
-      >
-        <span class="sr-only">
-          ({{ todo.done ? "finished" : "unfinished" }}) todo
-        </span>
-
-        <input
-          type="checkbox"
-          v-model="todo.done"
-          class="cursor-pointer"
-          @change="() => {emitUpdate(); announce(`marked todo as ${todo.done ? 'finished' : 'unfinished'}`) }"
-          ref="row-start"
-          :aria-label="
-            todo.done ? 'mark todo as unfinished' : 'mark todo as finished'
-          "
-        />
-
-        <span class="description" v-if="!isRowInEditMode(todo)">
-          {{ todo.description }}
-        </span>
-
-        <input
-          type="text"
-          class="border rounded px-2 description-editable"
-          v-model="editinigDescription"
-          ref="editableDescription"
-          @keypress.enter="doneEditMode()"
-          @keydown.esc="cancelEditMode()"
-          v-else
-          :aria-label="
-            'description of todo ' +
-            index +
-            ' press enter to confirm, escape to cancel'
-          "
-        />
-
-        <button
-          class="cursor-pointer"
-          v-if="!isRowInEditMode(todo)"
-          @click="putRowInEditMode(todo)"
-          aria-label="edit the description of todo"
-        >
-          edit
-        </button>
-        <template v-else>
-          <button
-            class="cursor-pointer"
-            @click="doneEditMode()"
-            aria-label="confirm your input"
-          >
-            save
-          </button>
-          <button
-            class="cursor-pointer"
-            @click="cancelEditMode()"
-            aria-label="cancel and restore previous description"
-          >
-            cancel
-          </button>
-        </template>
-
-        <button
-          class="cursor-pointer rotate-90"
-          :class="{ invisible: index === 0 }"
-          @click="moveUp(index, todo)"
-          :ref="'button-up-' + todo.id"
-          aria-label="move entry one up"
-        >
-          {{ "<" }}
-        </button>
-
-        <button
-          class="cursor-pointer rotate-90"
-          :class="{ invisible: index === todos.length - 1 }"
-          aria-label="move entry one down"
-          @click="moveDown(index, todo)"
-          :ref="'button-down-' + todo.id"
-        >
-          {{ ">" }}
-        </button>
-
-        <button
-          class="cursor-pointer"
-          @click="deleteTodo(index, todo)"
-          aria-label="remove entry"
-        >
-          x
-        </button>
-      </li>
-    </ul>
-
-    <!-- Add Button -->
-    <div class="flex justify-end" v-if="controls">
-      <button
-        class="border rounded p-2 cursor-pointer"
-        @click="addRow"
-        aria-label="add a new todo"
-      >
-        add
-      </button>
-    </div>
-  </div>
 </template>
 
 <script>
-export default {
-  props: {
-    todoList: {
-      required: true,
-      type: Array,
-    },
-    controls: {
-      type: Boolean,
-    },
-    filters: {
-      type: Boolean,
-    },
-  },
-  data() {
-    return {
-      rowInEditMode: null,
-      editinigDescription: "",
-      nextId: Math.max(...this.todoList.map((todo) => todo.id)) + 1,
-      setFilter: "none",
-      announcementMessage: "",
-      announcementDebounce: null,
-      todos: this.todoList,
-    };
-  },
-  computed: {
-    filteredTodos() {
-      return this.todos.filter(this.checkFilterOn);
-    },
-  },
-  methods: {
-    isRowInEditMode(todo) {
-      return this.rowInEditMode?.id === todo.id;
-    },
-    putRowInEditMode(todo) {
-      this.rowInEditMode = todo;
-      this.editinigDescription = todo.description;
-      Promise.resolve().then(() => {
-        this.saveTabIndex();
-        this.$refs.editableDescription?.[0]?.focus();
-      });
-    },
-    doneEditMode() {
-      this.rowInEditMode.description = this.editinigDescription;
-      if (this.rowInEditMode.isNew) {
-        delete this.rowInEditMode.isNew;
-      }
-      this.rowInEditMode = null;
-      this.emitUpdate();
-      this.restoreTabIndex();
-      this.announce("saved description for entry");
-    },
-    cancelEditMode() {
-      if (this.rowInEditMode.isNew) {
-        this.deleteTodo(this.rowInEditMode);
-      }
-      this.rowInEditMode = null;
-      this.restoreTabIndex();
-      this.announce("canceled your input");
-    },
-    addRow() {
-      const newTodo = {
-        id: this.nextId++,
-        description: "",
-        done: false,
-        isNew: true,
-      };
-      this.todos.push(newTodo);
-      this.putRowInEditMode(newTodo);
-      this.announce("Added new entry. Type in the description.");
-    },
-    deleteTodo(index, todo) {
-      this.todos.splice(this.todos.indexOf(todo), 1);
-      this.emitUpdate();
-      const nextIndex = index === this.todos.length ? index - 1 : index;
-      if (nextIndex >= 0) {
-        this.$nextTick(() => this.$refs["row-start"][nextIndex].focus());
-      }
-      this.announce(
-        `deleted entry at position ${index + 1}, you are now on the next entry`
-      );
-    },
-    moveUp(index, todo) {
-      if (index <= 0 || index >= this.todos.length) {
-        return;
-      }
-      this.todos.splice(index - 1, 2, this.todos[index], this.todos[index - 1]);
-      this.emitUpdate();
-      this.$nextTick(() => {
-        if (index <= 1) {
-          this.$refs["button-down-" + todo.id][0].focus();
-        } else {
-          this.$refs["button-up-" + todo.id][0].focus();
-        }
-      });
-      this.announce(
-        `moved entry up from position ${index + 1} to ${index}` +
-          (index <= 1 ? "reached top, you are now on the move down button" : "")
-      );
-    },
-    moveDown(index, todo) {
-      if (index < 0 || index + 1 >= this.todos.length) {
-        return;
-      }
-      this.todos.splice(index, 2, this.todos[index + 1], this.todos[index]);
-      this.emitUpdate();
-      this.$nextTick(() => {
-        if (index + 2 >= this.todos.length) {
-          this.$refs["button-up-" + todo.id][0].focus();
-        } else {
-          this.$refs["button-down-" + todo.id][0].focus();
-        }
-      });
-      this.announce(
-        `moved entry down from position ${index + 1} to ${index + 2}` +
-          (index + 2 >= this.todos.length
-            ? "reached bottom, you are now on the move up button"
-            : "")
-      );
-    },
-    emitUpdate() {
-      this.$emit("update", this.todos);
-    },
-    toggleFilter(value) {
-      if (this.setFilter === value) {
-        this.setFilter = "none";
-      } else {
-        this.setFilter = value;
-      }
-    },
-    checkFilterOn(todo) {
-      if (this.setFilter === "none") {
-        return true;
-      }
-      if (this.setFilter === "finished" && todo.done) {
-        return true;
-      }
-      if (this.setFilter === "unfinished" && !todo.done) {
-        return true;
-      }
-      return false;
-    },
-    saveTabIndex() {
-      this.lastFocusedElement = document.activeElement;
-    },
-    restoreTabIndex() {
-      this.lastFocusedElement.focus();
-    },
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-    // announce: debounce((message) => this.announcementMessage = message, 500),
-    announce(message) {
-      if (this.announcementDebounce !== null) {
-        clearTimeout(this.announcementDebounce);
-      }
-      this.announcementDebounce = setTimeout(() => {
-        this.announcementMessage = message;
-      }, 300);
+export default {
+    props: {
+        todoList: {
+            required: true,
+            type: Array,
+        },
+        controls: {
+            type: Boolean,
+        },
+        filters: {
+            type: Boolean,
+        },
     },
-  },
+    components: {
+        'v-icon': FontAwesomeIcon
+    },
+    data() {
+        return {
+            rowInEditMode: null,
+            editinigDescription: "",
+            nextId: Math.max(...this.todoList.map((todo) => todo.id)) + 1,
+            setFilter: "none",
+            announcementMessage: "",
+            announcementDebounce: null,
+            todos: this.todoList,
+        };
+    },
+    computed: {
+        filteredTodos() {
+            return this.todos.filter(this.checkFilterOn);
+        },
+    },
+    methods: {
+        isRowInEditMode(todo) {
+            return this.rowInEditMode?.id === todo.id;
+        },
+        putRowInEditMode(todo) {
+            this.rowInEditMode = todo;
+            this.editinigDescription = todo.description;
+            Promise.resolve().then(() => {
+                this.saveTabIndex();
+                this.$refs.editableDescription?.[0]?.focus();
+            });
+        },
+        doneEditMode() {
+            this.rowInEditMode.description = this.editinigDescription;
+            if (this.rowInEditMode.isNew) {
+                delete this.rowInEditMode.isNew;
+            }
+            this.rowInEditMode = null;
+            this.emitUpdate();
+            this.restoreTabIndex();
+            this.announce("saved description for entry");
+        },
+        cancelEditMode() {
+            if (this.rowInEditMode.isNew) {
+                this.deleteTodo(this.rowInEditMode);
+            }
+            this.rowInEditMode = null;
+            this.restoreTabIndex();
+            this.announce("canceled your input");
+        },
+        addRow() {
+            const newTodo = {
+                id: this.nextId++,
+                description: "",
+                done: false,
+                isNew: true,
+            };
+            this.todos.push(newTodo);
+            this.putRowInEditMode(newTodo);
+            this.announce("Added new entry. Type in the description.");
+        },
+        deleteTodo(index, todo) {
+            this.todos.splice(this.todos.indexOf(todo), 1);
+            this.emitUpdate();
+            const nextIndex = index === this.todos.length ? index - 1 : index;
+            if (nextIndex >= 0) {
+                this.$nextTick(() => this.$refs["row-start"][nextIndex].focus());
+            }
+            this.announce(
+                `deleted entry at position ${index + 1}, you are now on the next entry`
+            );
+        },
+        moveUp(index, todo) {
+            if (index <= 0 || index >= this.todos.length) {
+                return;
+            }
+            this.todos.splice(index - 1, 2, this.todos[index], this.todos[index - 1]);
+            this.emitUpdate();
+            this.$nextTick(() => {
+                if (index <= 1) {
+                    this.$refs["button-down-" + todo.id][0].focus();
+                } else {
+                    this.$refs["button-up-" + todo.id][0].focus();
+                }
+            });
+            this.announce(
+                `moved entry up from position ${index + 1} to ${index}` +
+                (index <= 1 ? "reached top, you are now on the move down button" : "")
+            );
+        },
+        moveDown(index, todo) {
+            if (index < 0 || index + 1 >= this.todos.length) {
+                return;
+            }
+            this.todos.splice(index, 2, this.todos[index + 1], this.todos[index]);
+            this.emitUpdate();
+            this.$nextTick(() => {
+                if (index + 2 >= this.todos.length) {
+                    this.$refs["button-up-" + todo.id][0].focus();
+                } else {
+                    this.$refs["button-down-" + todo.id][0].focus();
+                }
+            });
+            this.announce(
+                `moved entry down from position ${index + 1} to ${index + 2}` +
+                (index + 2 >= this.todos.length
+                    ? "reached bottom, you are now on the move up button"
+                    : "")
+            );
+        },
+        emitUpdate() {
+            this.$emit("update", this.todos);
+        },
+        toggleFilter(value) {
+            if (this.setFilter === value) {
+                this.setFilter = "none";
+            } else {
+                this.setFilter = value;
+            }
+        },
+        checkFilterOn(todo) {
+            if (this.setFilter === "none") {
+                return true;
+            }
+            if (this.setFilter === "finished" && todo.done) {
+                return true;
+            }
+            if (this.setFilter === "unfinished" && !todo.done) {
+                return true;
+            }
+            return false;
+        },
+        saveTabIndex() {
+            this.lastFocusedElement = document.activeElement;
+        },
+        restoreTabIndex() {
+            this.lastFocusedElement.focus();
+        },
+
+        // announce: debounce((message) => this.announcementMessage = message, 500),
+        announce(message) {
+            if (this.announcementDebounce !== null) {
+                clearTimeout(this.announcementDebounce);
+            }
+            this.announcementDebounce = setTimeout(() => {
+                this.announcementMessage = message;
+            }, 300);
+        },
+    },
 };
 </script>
 
 <style scoped>
 .row-container {
-  @apply flex flex-row gap-3;
-  @apply p-4 border rounded;
+    @apply flex flex-row gap-3;
+    @apply p-4 border rounded;
 }
 
 .description {
-  @apply flex-1;
+    @apply flex-1;
 }
 
-.done > .description {
-  @apply line-through;
+.done>.description {
+    @apply line-through;
 }
 
 .description-editable {
-  @apply flex-1;
+    @apply flex-1;
 }
 
 .filter-button {
-  @apply border rounded bg-gray-100 px-2;
+    @apply border rounded bg-gray-100 px-2;
 }
 
 .filter-button.active {
-  @apply bg-blue-600 text-white;
+    @apply bg-blue-600 text-white;
 }
 
 .sr-only {
-  /* https://webaim.org/techniques/css/invisiblecontent/ */
-  position: absolute;
-  left: -10000px;
-  top: auto;
-  width: 1px;
-  height: 1px;
-  overflow: hidden;
+    /* https://webaim.org/techniques/css/invisiblecontent/ */
+    position: absolute;
+    left: -10000px;
+    top: auto;
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 
 import "./assets/main.css";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faAngleDown, faAngleUp, faEdit, faPlus, faSave, faTrashAlt, faUndo } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faAngleUp, faAngleDown, faUndo, faTrashAlt, faEdit, faSave, faPlus);
+
 new Vue({
-  render: (h) => h(App),
+  render: (h) => h(App)
 }).$mount("#app");
